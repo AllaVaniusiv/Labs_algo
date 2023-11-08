a34e150fffa6a1048bd0fbbe3ae7a4a9fa0a78fe
AllaVaniusiv <allavaniusiv@gmail.com>
2023-11-08 19:24:27 +0200

    adding lab4

--- Lab4/lab4.py
+++ Lab4/lab4.py
@@ -1,38 +1,39 @@
 from collections import deque
 
-def find_root_vertex(graph):
-    def bfs(start_vertex):
+def read_graph_from_file(filename):
+    graph = {}
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            parts = line.strip().split()
+            vertex = int(parts[0])
+            neighbors = [int(x) for x in parts[1:]]
+            graph[vertex] = neighbors
+    return graph
+
+def find_root(graph):
+    def bfs(graph, start):
         visited = set()
-        queue = deque([start_vertex])
+        queue = deque([start])
         while queue:
-            vertex = queue.popleft() 
+            vertex = queue.popleft()
             visited.add(vertex)
-            for neighbor in graph[vertex]:
+            for neighbor in graph.get(vertex, []):
                 if neighbor not in visited:
                     queue.append(neighbor)
         return visited
 
-    roots = []
     for vertex in graph:
-        if len(bfs(vertex)) == len(graph):
-            roots.append(vertex)
-
-    if not roots:
-        return -1
-    else:
-        return roots[0]
-
-graph = {}
-with open("input.txt", "r") as file:
-    for line in file:
-        u, v = map(int, line.strip().split())
-        if u in graph:
-            graph[u].append(v)
-        else:
-            graph[u] = [v]
-
-root_vertex = find_root_vertex(graph)
+        if not any(vertex in bfs(graph, v) for v in graph if v != vertex):
+            return vertex
+    return -1
 
 
-with open("output.txt", "w") as file:
-    file.write(str(root_vertex))
+input_file = "input.txt"
+output_file = "output.txt"
+
+graph = read_graph_from_file(input_file)
+root = find_root(graph)
+
+with open(output_file, 'w') as file:
+    file.write(str(root))
